9216062f8d2fea65c241e025523bf4b6ac5e7594
Mitchel Klijn <mitchelklijn2004@gmail.com>
2023-05-17 11:05:34 +0200

    Camera rotation optimized, camera zoom made

--- Project Steel Plague/Assets/Scripts/Player Input/CameraMovement.cs
+++ Project Steel Plague/Assets/Scripts/Player Input/CameraMovement.cs
@@ -12,22 +12,39 @@ public class CameraMovement : MonoBehaviour
     private Vector3 direction;
     private Vector3 overshoot;
 
+    [SerializeField] private float zoomDistance;
+    private float defaultDistance;
+
+    private bool inZoom;
+
+    private void Start()
+    {
+        defaultDistance = Camera.main.transform.position.z;
+    }
+
     private void Update()
     {
         if(direction != Vector3.zero)
         {
             overshoot = direction;
-            transform.eulerAngles += speed * Time.deltaTime * new Vector3(direction.y, -direction.x);
+            transform.localEulerAngles += speed * Time.deltaTime * new Vector3(direction.y, -direction.x);
         }
         else
         {
             overshoot = Vector3.Lerp(overshoot, Vector3.zero, Time.deltaTime);
-            transform.eulerAngles += stoppingSpeed * Time.deltaTime * new Vector3(overshoot.y, -overshoot.x);
+            transform.localEulerAngles += stoppingSpeed * Time.deltaTime * new Vector3(overshoot.y, -overshoot.x);
         }
     }
 
     public void OnMove(InputValue value)
     {
         direction = value.Get<Vector2>();
+    }
+
+    public void OnZoom(InputValue value)
+    {
+        inZoom = value.Get<float>() > 0 || (value.Get<float>() >= 0 && inZoom);
+
+        Camera.main.transform.localPosition = inZoom ? new Vector3(0, 0, zoomDistance) : new Vector3(0, 0, defaultDistance);
     }
 }
