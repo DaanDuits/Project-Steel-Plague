f73b4d53918d334624503482bcaba26e11529159
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2023-06-12 11:22:10 +0200

    Re-added progress bar with assets.

--- Project Steel Plague/Assets/Scripts/ProgressHandler.cs
+++ Project Steel Plague/Assets/Scripts/ProgressHandler.cs
@@ -8,8 +8,15 @@ public class ProgressHandler : MonoBehaviour
     public BotHandler robot;
     public Transform progressPanel;
 
+    [SerializeField] private GameObject topBar;
+    [SerializeField] private GameObject bottomBar;
+
     private void Start()
     {
+
+        topBar.SetActive(false);
+        bottomBar.SetActive(false);
+
         RectTransform panelRectTransform = progressPanel.GetComponent<RectTransform>();
 
         panelRectTransform.sizeDelta = new Vector2 (800, 0);
@@ -26,35 +33,37 @@ public class ProgressHandler : MonoBehaviour
 
         if (robot.bots >= 1 && robot.bots <= 99)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 10);
+            panelRectTransform.sizeDelta = new Vector2(50, 10);
+            bottomBar.SetActive(true);
         }
         else if (robot.bots >= 100 && robot.bots <= 499)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 50);
+            panelRectTransform.sizeDelta = new Vector2(50, 30);
         }
         else if (robot.bots >= 500 && robot.bots <= 999)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 100);
+            panelRectTransform.sizeDelta = new Vector2(50, 50);
         }
         else if (robot.bots >= 1000 && robot.bots <= 4999)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 150);
+            panelRectTransform.sizeDelta = new Vector2(50, 75);
         }
         else if (robot.bots >= 5000 && robot.bots <= 9999)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 200);
+            panelRectTransform.sizeDelta = new Vector2(50, 100);
         }
         else if (robot.bots >= 10000 && robot.bots <= 49999)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 250);
+            panelRectTransform.sizeDelta = new Vector2(50, 150);
         }
         else if (robot.bots >= 50000 && robot.bots <= 99999)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 300);
+            panelRectTransform.sizeDelta = new Vector2(50, 200);
         }
         else if (robot.bots >= 100000)
         {
-            panelRectTransform.sizeDelta = new Vector2(800, 400);
+            panelRectTransform.sizeDelta = new Vector2(50, 230);
+            topBar.SetActive(true);
         }
     }
 }
