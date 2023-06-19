69ea0f11fa379508ebfc79d77dca51fcc9ca9722
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2023-06-19 11:33:37 +0200

    Implemented humans and robots (Fixed a bug with the continue

--- Project Steel Plague/Assets/Scripts/ProgressHandler.cs
+++ Project Steel Plague/Assets/Scripts/ProgressHandler.cs
@@ -10,11 +10,25 @@ public class ProgressHandler : MonoBehaviour
     [SerializeField] private GameObject winMenu;
     [SerializeField] private Collider planet;
     [SerializeField] private Collider[] factories;
+    [SerializeField] private GameObject[] humans;
+    [SerializeField] private GameObject[] robots;
 
     [SerializeField] Slider slider;
 
     private void Start()
     {
+        robots[0].SetActive(false);
+        robots[1].SetActive(false);
+        robots[2].SetActive(false);
+        robots[3].SetActive(false);
+        robots[4].SetActive(false);
+
+        humans[0].SetActive(true);
+        humans[1].SetActive(true);
+        humans[2].SetActive(true);
+        humans[3].SetActive(true);
+        humans[4].SetActive(true);
+
         slider.maxValue = 200;
         winMenu.SetActive(false);
     }
@@ -26,18 +40,28 @@ public class ProgressHandler : MonoBehaviour
         switch (robot.bots)
         {
             case 1:
+                robots[0].SetActive(true);
+                humans[0].SetActive(false);
                 NewMilestone();
                 break;
             case 50:
+                robots[1].SetActive(true);
+                humans[1].SetActive(false);
                 NewMilestone();
                 break;
             case 100:
+                robots[2].SetActive(true);
+                humans[2].SetActive(false);
                 NewMilestone();
                 break;
             case 150:
+                robots[3].SetActive(true);
+                humans[3].SetActive(false);
                 NewMilestone();
                 break;
             case 200:
+                robots[4].SetActive(true);
+                humans[4].SetActive(false);
                 NewMilestone();
                 Debug.Log("Game Over");
                 winMenu.SetActive(true);
