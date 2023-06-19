b8ecd2227cbf4c914d9ad9d5751270491cc17782
WizelfMike <70898423+WizelfMike@users.noreply.github.com>
2023-06-19 11:36:15 +0200

    Merge branch 'mikey' into develop

--- Project Steel Plague/Assets/Scripts/ProgressHandler.cs
+++ Project Steel Plague/Assets/Scripts/ProgressHandler.cs
@@ -7,11 +7,30 @@ public class ProgressHandler : MonoBehaviour
 {
     public BotHandler robot;
 
+    [SerializeField] private GameObject winMenu;
+    [SerializeField] private Collider planet;
+    [SerializeField] private Collider[] factories;
+    [SerializeField] private GameObject[] humans;
+    [SerializeField] private GameObject[] robots;
+
     [SerializeField] Slider slider;
 
     private void Start()
     {
-        slider.maxValue = 100;
+        robots[0].SetActive(false);
+        robots[1].SetActive(false);
+        robots[2].SetActive(false);
+        robots[3].SetActive(false);
+        robots[4].SetActive(false);
+
+        humans[0].SetActive(true);
+        humans[1].SetActive(true);
+        humans[2].SetActive(true);
+        humans[3].SetActive(true);
+        humans[4].SetActive(true);
+
+        slider.maxValue = 200;
+        winMenu.SetActive(false);
     }
 
     private void Update()
@@ -21,34 +40,39 @@ public class ProgressHandler : MonoBehaviour
         switch (robot.bots)
         {
             case 1:
+                robots[0].SetActive(true);
+                humans[0].SetActive(false);
+                NewMilestone();
+                break;
+            case 50:
+                robots[1].SetActive(true);
+                humans[1].SetActive(false);
                 NewMilestone();
                 break;
             case 100:
+                robots[2].SetActive(true);
+                humans[2].SetActive(false);
                 NewMilestone();
                 break;
-            case 250:
+            case 150:
+                robots[3].SetActive(true);
+                humans[3].SetActive(false);
                 NewMilestone();
                 break;
-            case 500:
+            case 200:
+                robots[4].SetActive(true);
+                humans[4].SetActive(false);
                 NewMilestone();
-                break;
-            case 750:
-                NewMilestone();
-                break;
-            case 1000:
-                NewMilestone();
-                break;
-            case 2500:
-                NewMilestone();
-                break;
-            case 5000:
-                NewMilestone();
-                break;
-            case 7500:
-                NewMilestone();
-                break;
-            case 10000:
-                NewMilestone();
+                Debug.Log("Game Over");
+                winMenu.SetActive(true);
+                planet.enabled = false;
+
+                foreach (Collider c in factories)
+                {
+                    c.enabled = false;
+                }
+
+                Time.timeScale = 0;
                 break;
         }
     }
